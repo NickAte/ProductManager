d32e5b4ae57105ba9c8068ffc21b9e003a07c6d8
NickAte <72125393+NickAte@users.noreply.github.com>
2022-09-06 12:49:58 +0300

    Test

---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ProductManagerTest {
+    ProductRepository repo = new ProductRepository();
+    ProductManager manager = new ProductManager(repo);
+
+    Book book1 = new Book(1, "The One", 200, "Ivan");
+    Book book2 = new Book(22, "One of Two", 300, "Pavel");
+    Book book3 = new Book(3, "Two Thrones", 100, "Oleg");
+
+    @Test
+    public void TwoBooks() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("One");
+        Product[] expected = {book1, book2};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void OneBooks() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("The");
+        Product[] expected = {book1};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void NoBooks() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("Ring");
+        Product[] expected = {};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FindAll() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.repo.findAll();
+        Product[] expected = {book1, book2, book3};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveById() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.repo.removeById(1);
+        Product[] expected = {book2, book3};
+
+        assertArrayEquals(expected, actual);
+    }
+}
